c8373868ec0e35f40184d4a86ab26959cb94401a
zoud <zoud@rose-hulman.edu>
2019-03-19 21:25:03 -0400

    DONE

--- src/problem1.py
+++ src/problem1.py
@@ -3,13 +3,13 @@ Exam 1, problem 1.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Deng Zou.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 # -----------------------------------------------------------------------------
-# TODO: 2. Right-click on the  src  folder and
+# Done: 2. Right-click on the  src  folder and
 #              Mark Directory as ... Sources Root,
 #          if you have not already done so.
 # -----------------------------------------------------------------------------
@@ -110,7 +110,6 @@ def problem1(circle, rectangle, color, length, window):
 
       Note: Attach the rg.Line AFTER attaching the rg.Circle and rg.Rectangle.
       Must render but   ** NOT close **   the window.
-
     Type hints:
       :type circle:    rg.Circle
       :type rectangle: rg.Rectangle
@@ -118,10 +117,24 @@ def problem1(circle, rectangle, color, length, window):
       :type window:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
+    # Done: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    line1=rg.Line(circle.center,rectangle.get_center())
+    line1.color=color
+    line1.thickness=circle.outline_thickness
+    line1.attach_to(window)
 
+    startpoint=rg.Point(line1.get_midpoint().x,line1.get_midpoint().y+length/2)
+    endpoint=rg.Point(line1.get_midpoint().x,line1.get_midpoint().y-length/2)
+    line2=rg.Line(startpoint,endpoint)
+    line2.color=circle.fill_color
+    line2.thickness=circle.outline_thickness+rectangle.outline_thickness
+    line2.attach_to(window)
+
+    window.render()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
